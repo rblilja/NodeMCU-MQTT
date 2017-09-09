52ac449883704d7ac77af7b49a413b0d0bb01ba9
Robin Lilja <robin.lilja@gmail.com>
2017-09-09 16:12:10 +0200

    Update _init.lua

--- _init.lua
+++ _init.lua
@@ -15,31 +15,20 @@ am2320.setup()
 function sample()
 	
 	if wireless.is_ready() == true and mqttd.is_ready() == true then
-	
-		--t = {}
-		--rh = {}
-	
-		-- Take three samples (unrolled loop)
-		--rh[1], t[1] = am2320.read()
-		--rh[2], t[2] = am2320.read()
-		--rh[3], t[3] = am2320.read()
 		
-		--t_mean = ((t[1] + t[2] + t[3]) / 3) / 10
-		--rh_mean = ((rh[1] + rh[2] + rh[3]) / 3) / 10
+		rh, t = am2320.read()
 		
-		rh_mean, t_mean = am2320.read()
+		rh = rh / 10
+		t = t / 10
 		
-		rh_mean = rh_mean / 10
-		t_mean = t_mean / 10
-		
-		print("\n\tAM2320 - SAMPLED".."\n\t\Temperature: "..string.format("%s degrees C", t_mean).."\n\t\RH: "..string.format("%s%%", rh_mean))
+		print("\n\tAM2320 - SAMPLED".."\n\t\Temperature: "..string.format("%s degrees C", t).."\n\t\RH: "..string.format("%s%%", rh))
 
-		payload = "{ \"temp\":"..(t_mean)..", \"RH\":"..(rh_mean).." }"
+		payload = "{ \"temp\":"..(t)..", \"RH\":"..(rh).." }"
 		
 		mqttd.publish("nodemcu/am2320", payload)
 	else
 	
-		print("\n\tAM2320 - FAILED")
+		print("\n\tMQTT - Not Ready")
 	end
 end	
 
